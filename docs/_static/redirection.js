5e33b748c65b9e978bde4fa3ddf06c61b7c9c830
Robert Winkler <rwinkler@antmicro.com>
2020-09-23 13:57:37 +0200

    Add basic redirection to new RTD address

---
+++ docs/_static/redirection.js
@@ -0,0 +1,2 @@
+alert("The project name has been changed from sphinxcontrib-verilog-diagrams to sphinxcontrib-hdl-diagrams.\n\nYou will be redirected to the new website address")
+window.location.replace('https://sphinxcontrib-hdl-diagrams.readthedocs.io/en/latest/');
--- docs/conf.py
+++ docs/conf.py
@@ -251,3 +251,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# Redirection setup
+html_js_files = ['redirection.js']
